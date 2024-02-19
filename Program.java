f59455bba0e7582355a993f6863047ce7b41637b
Zheshard <antonzestkov489@gmail.com>
2024-02-19 15:02:20 +0300

    homework

---
+++ Program.java
@@ -0,0 +1,26 @@
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class Program {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>(4);
+
+        String res = hashMap.put("+79005554433", "Андрей");
+        res = hashMap.put("+79005554432", "Алекскей");
+        res = hashMap.put("+79005554432", "Дарья1");
+        res = hashMap.put("+79005554433", "Дарья2");
+        res = hashMap.put("+79005554434", "Дарья3");
+        res = hashMap.put("+79005554435", "Дарья4");
+        res = hashMap.put("+79005554436", "Дарья5");
+        res = hashMap.put("+79005554437", "Дарья6");
+        res = hashMap.put("+79005554438", "Дарья7");
+        res = hashMap.put("+79005554439", "Дарья8");
+
+        System.out.println(hashMap.get("+79005554437"));
+
+        for (HashMap.Entity element : hashMap) {
+            System.out.println(element.key + " - " + element.value);
+        }
+    }
+}
